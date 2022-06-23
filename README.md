effbcc8d3e171e747742bc93abd3cabb78ba23c6
ichlubna <ichlubna@fit.vutbr.cz>
2022-06-23 10:20:29 +0200

    Multiple files

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Any file to image convertor
 The script read the input file in a binary mode and saves the bytes in an image file.
 
+Examples:
 ```
-python anythingToImage inputFile output.png
+python anythingToImage inputFile output.png rgb|gray 8|16
+python anythingToImage inputImage.png outputFile
+```
+
+Can produce multiple images limited by resolution:
+```
+python anythingToImage inputFile output.png rgb|gray 8|16 maxResolution
+python anythingToImage inputFolder outputFile
 ```
--- anythingToImage.py
+++ anythingToImage.py
@@ -6,60 +6,80 @@ import numpy
 from PIL import Image
 
 def toImage():
+    path, fileName = os.path.split(sys.argv[2])
+
+    dimension = -1
+    if len(sys.argv) > 5:
+        dimension = int(sys.argv[5])
+
     file = open(sys.argv[1], "rb")
-    size = os.stat(sys.argv[1]).st_size
     depth = 3
     if sys.argv[3] == "gray":
         depth = 1
-    dimension = math.ceil(math.sqrt(size/depth))
-    result = numpy.zeros(dimension*dimension*depth, numpy.uint8)
-    reads = 1
-    if sys.argv[4] == "16":
-        dimension = math.ceil(math.sqrt(math.ceil(size/2)/depth))
-        result = numpy.zeros(dimension*dimension*depth, numpy.uint16)
-        reads = 2
 
-    byte = file.read(1)
-    i = 0
-    while byte:
-        byteList = [0,0]
-        for j in range(reads):
-            byteList[j] = int.from_bytes(byte, "little")
-            byte = file.read(1)
-        result[i] = int.from_bytes(byteList, "little")
-        #print(chr(result[i]))
-        i += 1
+    size = os.stat(sys.argv[1]).st_size
+    if dimension == -1:
+        dimension = math.ceil(math.sqrt(size/depth))
+        if sys.argv[4] == "16":
+            dimension = math.ceil(math.sqrt(math.ceil(size/2)/depth))
+    sizeLimit = dimension*dimension*depth
 
-    result = numpy.reshape(result, (dimension, dimension, depth))
-    cv2.imwrite(sys.argv[2], result)
+    fileCount = int(math.ceil(float(size)/sizeLimit))
+    for b in range(0, fileCount):
+        result = numpy.zeros(dimension*dimension*depth, numpy.uint8)
+        reads = 1
+        if sys.argv[4] == "16":
+            result = numpy.zeros(dimension*dimension*depth, numpy.uint16)
+            reads = 2
+
+        byte = file.read(1)
+        for i in range(0, len(result)):
+            byteList = [0,0]
+            for j in range(reads):
+                byteList[j] = int.from_bytes(byte, "little")
+                byte = file.read(1)
+            result[i] = int.from_bytes(byteList, "little")
+
+        result = numpy.reshape(result, (dimension, dimension, depth))
+        cv2.imwrite(os.path.join(path, str(b)+fileName), result)
 
 def fromImage():
-    im = Image.open(sys.argv[1])
+    path, fileName = os.path.split(sys.argv[1])
+    files = [ fileName ]
+    if os.path.isdir(sys.argv[1]):
+        files = sorted(os.listdir(sys.argv[1]))
+    im = Image.open(os.path.join(path,files[0]))
     mode = cv2.IMREAD_GRAYSCALE
     depth = 1
     if im.mode == "RGB":
         mode = cv2.IMREAD_COLOR
         depth = 3
-    img = cv2.imread(sys.argv[1], mode)
-    height = img.shape[0]
-    width = img.shape[1]
-    data = [0] * width*height*depth
-    l = 0
-    for i in range(0, height):
-         for j in range(0, (width)):
-            if im.mode == "RGB":
-                for k in range(0,depth):
-                   data[l] = img[i,j,k]
+
+    allData = []
+    for inputFile in files:
+        img = cv2.imread(os.path.join(path, inputFile), mode)
+        height = img.shape[0]
+        width = img.shape[1]
+        data = [0] * width*height*depth
+        l = 0
+        for i in range(0, height):
+             for j in range(0, (width)):
+                if im.mode == "RGB":
+                    for k in range(0,depth):
+                       data[l] = img[i,j,k]
+                       l += 1
+                else:
+                   data[l] = img[i,j]
                    l += 1
-            else:
-               data[l] = img[i,j]
-               l += 1
+        allData += data
+
     file = open(sys.argv[2], "wb")
-    fileByteArray = bytearray(data)
+    fileByteArray = bytearray(allData)
     file.write(fileByteArray)
 
 if sys.argv[1] == "-h" or sys.argv[1] == "--help":
-    print("Arguments: inputFile, outputFile, mode: gray|rgb, bit 8|16 \nIf only first two arguments are used, the reverse image to file conversion is performed")
+    print("Arguments: inputFile, outputFile, mode: gray|rgb, bit 8|16, image resolution (one dimension) \nIf only first two arguments are used, the reverse image to file conversion is performed \nThe input can also be folder in case of decoding")
+    exit(0)
 
 if len(sys.argv) > 3:
     toImage()
